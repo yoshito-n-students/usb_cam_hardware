1a70f2e363dc37215d92277db48e05d0e8bda4b2
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-12-22 09:25:33 +0900

    refactor h264&mjpeg controllers by using common template

--- usb_cam_controllers/src/usb_cam_controllers.cpp
+++ usb_cam_controllers/src/usb_cam_controllers.cpp
@@ -1,8 +1,7 @@
 #include <pluginlib/class_list_macros.h>
 
 #include "usb_cam_controllers/camera_info_controller.hpp"
-#include "usb_cam_controllers/h264_controller.hpp"
-#include "usb_cam_controllers/mjpeg_controller.hpp"
+#include "usb_cam_controllers/video_controllers.hpp"
 #include "usb_cam_controllers/packet_controller.hpp"
 
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::CameraInfoController,
