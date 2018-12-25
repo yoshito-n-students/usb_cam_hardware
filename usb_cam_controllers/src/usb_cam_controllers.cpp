3a662b47f7ab436a4e33ccce423652b8c054cd1e
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-12-25 11:23:08 +0900

    add controllers for format conversion (ex. uyvy -> rgb)

--- usb_cam_controllers/src/usb_cam_controllers.cpp
+++ usb_cam_controllers/src/usb_cam_controllers.cpp
@@ -2,6 +2,7 @@
 
 #include "usb_cam_controllers/camera_info_controller.hpp"
 #include "usb_cam_controllers/decoding_controllers.hpp"
+#include "usb_cam_controllers/format_controllers.hpp"
 #include "usb_cam_controllers/packet_controller.hpp"
 
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::CameraInfoController,
@@ -9,3 +10,5 @@ PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::CameraInfoController,
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::H264Controller, controller_interface::ControllerBase);
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::MjpegController, controller_interface::ControllerBase);
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::PacketController, controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::UYVYController, controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::YUYVController, controller_interface::ControllerBase);
