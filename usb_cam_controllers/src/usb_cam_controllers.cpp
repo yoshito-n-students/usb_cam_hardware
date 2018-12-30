a3a1e083b9d5573c11769af24036ecd6957fc3dc
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-12-30 11:17:05 +0900

    refactor packet controllers; - rename PacketController to CompressedPacketController - add new PacketController which publishes sensor_msgs::Image

--- usb_cam_controllers/src/usb_cam_controllers.cpp
+++ usb_cam_controllers/src/usb_cam_controllers.cpp
@@ -4,10 +4,11 @@
 #include "usb_cam_controllers/camera_info_controller.hpp"
 #include "usb_cam_controllers/decoding_controllers.hpp"
 #include "usb_cam_controllers/format_controllers.hpp"
-#include "usb_cam_controllers/packet_controller.hpp"
+#include "usb_cam_controllers/packet_controllers.hpp"
 
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::CameraInfoController,
                        controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::CompressedPacketController, controller_interface::ControllerBase);
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::H264Controller, controller_interface::ControllerBase);
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::MjpegController, controller_interface::ControllerBase);
 PLUGINLIB_EXPORT_CLASS(usb_cam_controllers::PacketController, controller_interface::ControllerBase);
